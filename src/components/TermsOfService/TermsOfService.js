d02c8467238ad8403188f4d4fed4228edcbe5c31
Grayson Sharpe <sto2979@gmail.com>
2022-03-14 01:43:46 -0400

    Updated Privacy Policy and Terms of Service

--- src/components/TermsOfService/TermsOfService.js
+++ src/components/TermsOfService/TermsOfService.js
@@ -11,52 +11,83 @@ const TermsOfService = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: November 22, 2019</p>
+    <p className={css.lastUpdated}>Last updated: March 14, 2022</p>    
 
-      <p>
-        Thank you for using HoopDaMentals! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+    <p>
+      Hoopdamentals is a service that allows its users to change favors and items and
+      communicate with each other. The service may only be used in accordance with these
+      terms of use. The service provider reserves the right to change these terms of use
+      if required. Valid terms of use can be found on app.hoopdamentals.com Terms of use
+      section.
+    </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+    <h2>Rights of Content</h2>
+    <p>
+      The users themselves retain the right to all text, pictures and other content that
+      they create in the service. The users allow others to utilize the content in
+      accordance with the nature of the service and furthermore allow the service
+      provider to file information and data and make changes that are necessary for the
+      service or the study, however other rights are not transferred from the users,
+      unless specifically otherwise agreed. The responsibility of the content lies with
+      the user, who has produced it to the service. The service provider has the right
+      to remove any material when it deems it necessary.
+    </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-      </p>
+    <h2>
+      The Site is a Venue and We are Not a Party to any Rental Agreement or other
+      Transaction Between Users of the Site.
+    </h2>
+    <p>
+      We urge all users to be responsible about their use of this Site and any
+      transaction entered into as a result of either listing a gymnasium or renting a
+      gymnasium. We do not own or manage, nor can we contract for, any gymnasium listed
+      on this Site. The Site provides an on-line marketplace to allow the owner or a
+      property manager or duly authorized agent of the owner of a gymnasium to offer for
+      rent on this Site a specific gymnasium to potential renters or renters. We are not
+      a party to any rental or other agreement between users. You acknowledge and agree
+      that you may be required to enter into one or more separate agreements, waivers or
+      terms and conditions before renting a gymnasium.
+    </p>
 
-      <h2>3 At vero eos et accusamus</h2>
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
-      </p>
-    </div>
+    <p>
+      Users agree that they are responsible for and agree to abide by all laws, rules,
+      ordinances, or regulations applicable to the listing or renting of a gymnasium,
+      including but not limited to any and all laws, rules, ordinances, regulations or
+      other requirements relating to taxes, credit cards, data and privacy, permits or
+      license requirements, zoning ordinances, safety compliance and compliance with all
+      anti-discrimination and fair housing laws, as applicable. Please be aware that,
+      even though we are not a party to any rental transaction and assume no liability
+      for legal or regulatory compliance pertaining to rental gymnasiums listed on the
+      Site, there may be circumstances where we are nevertheless legally obligated (as
+      we may determine in our sole discretion) to provide information relating to your
+      listing in order to comply with requests from governmental bodies in relation to
+      investigations, litigation or administrative proceedings, and we may choose to
+      comply with such obligations in our sole discretion.
+    </p>
+
+    <h2>Disclaimer</h2>
+    <p>
+      No guarantees of the functioning of the Hoopdamentals service are given. The users
+      are themselves responsible for their actions in the service and they should
+      estimate the reliability of other users before dealing with them. The service
+      provider can under no circumstances be liable for damage that is caused to the
+      user. The user may not store any information or data in the service, and expect it
+      to remain there.
+    </p>
+
+    <h2>The Removal of a User</h2>
+    <p>
+      The service provider has the right to remove any users from Hoopdamentals and
+      terminate their right of use of the service without any specific reason and
+      without being liable for compensation.
+    </p>
+
+    <h2>Applicable Jurisdiction</h2>
+    <p>
+      The jurisdiction that is applicable in this service and these terms of use is that
+      of Tennessee, unless something else is required by binding law.
+    </p>
+  </div>
   );
 };
 
