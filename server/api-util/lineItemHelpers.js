277de63f847acdb316580ceba4b8cdc4c82bccdf
Grayson Sharpe <sto2979@gmail.com>
2022-05-03 09:47:31 -0400

    Removed logging

--- server/api-util/lineItemHelpers.js
+++ server/api-util/lineItemHelpers.js
@@ -179,13 +179,9 @@ exports.calculateTransactionFee = lineItems => {
 };
 
 exports.calculateProcessingFee = lineItems => {
-  console.log('calculateProcessingFee');
   const totalPrice = this.calculateTotalFromLineItems(lineItems);
-  console.log(totalPrice);
   const grossPrice = totalPrice.amount * 1.05;
-  console.log(grossPrice);
   const processingFee = (grossPrice * 0.029 + 30) * -1;
-  console.log(processingFee);
   return new Money(processingFee, totalPrice.currency);
 };
 
