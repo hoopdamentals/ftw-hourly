7be0aefaae7e6657b0b3660bc15584c427fa8f27
Grayson Sharpe <sto2979@gmail.com>
2022-05-20 07:17:00 -0400

    Added minimum $1.50 transaction fee

--- server/api-util/lineItemHelpers.js
+++ server/api-util/lineItemHelpers.js
@@ -174,13 +174,14 @@ exports.calculateTotalForCustomer = lineItems => {
 exports.calculateTransactionFee = lineItems => {
   const totalPrice = this.calculateTotalFromLineItems(lineItems);
 
-  const commission = totalPrice.amount * 0.06;
+  const commission = Math.max(totalPrice.amount * 0.06, 150);
   return new Money(commission, totalPrice.currency);
 };
 
 exports.calculateProcessingFee = lineItems => {
   const totalPrice = this.calculateTotalFromLineItems(lineItems);
-  const grossPrice = totalPrice.amount * 1.06;
+  const commission = Math.max(totalPrice.amount * 0.06, 150);
+  const grossPrice = totalPrice.amount + commission;
   const processingFee = (grossPrice * 0.029 + 30) * -1;
   return new Money(processingFee, totalPrice.currency);
 };
